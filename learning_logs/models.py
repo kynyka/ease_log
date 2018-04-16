a877a94c9ae7b8e7315e51419da70a4a82541f9c
kynyka <congsu.yeh@gmail.com>
2018-04-16 19:20:19 +0800

    define a model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    '''用户学习的主题'''
+    text = models.CharField(max_length=200)  # charfield必须告知django该在数据库中预留多少空间
+    date_added = models.DateTimeField(auto_now_add=True)  # 自动设置成当前日期和时间;见官网: ... ref/models/fields/
+
+    def __str__(self):
+        '''返回模型的字符串表示'''
+        return self.text
