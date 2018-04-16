04b52a46b8b715ef2bd7d83da84ec0caeb06e047
kynyka <congsu.yeh@gmail.com>
2018-04-16 19:43:15 +0800

    rev:define a model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,6 +9,6 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)  # charfield必须告知django该在数据库中预留多少空间
     date_added = models.DateTimeField(auto_now_add=True)  # 自动设置成当前日期和时间;见官网: ... ref/models/fields/
 
-    def __str__(self):
+    def __unicode__(self):  # Py 3.X用__str__()
         '''返回模型的字符串表示'''
         return self.text
