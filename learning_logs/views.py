4e710f5078d1df443d52f5ac6bbb16f64828fc4a
kynyka <congsu.yeh@gmail.com>
2018-04-19 09:52:38 +0800

    rev: add comments

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     '''学习笔记的主页'''
-    return render(request, 'learning_logs/index.html')
+    return render(request, 'learning_logs/index.html')  # 第1个实参为原始请求对象,第2个实参为可用于创建网页的模板
