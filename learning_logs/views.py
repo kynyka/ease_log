1e1612f7b44c2210d59363d2305db53e51d2f366
kynyka <congsu.yeh@gmail.com>
2018-04-22 15:05:49 +0800

    视图函数: new_topic()

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,8 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponseRedirect #这个URL模式将请求交给视图函数new_topic,接下来我们将编写这个函数。
+from django.core.urlresolvers import reverse
 
 from .models import Topic  # 首先导入与所需数据相关联的模型
+from .forms import TopicForm
 
 # Create your views here.
 def index(request):
@@ -22,3 +25,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')  #  date_added 前面的减号指定按降序排列,即先显示最近的条目
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    '''添加新主题'''
+    if request.method != 'POST':
+        # 未提交数据：创建一个新表单
+        form = TopicForm()
+    else:
+        # POST提交的数据,对数据进行处理
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))  # 函数reverse()根据指定的URL模型确定URL,这意味着Django将在页面被请求时生成URL
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
