36e2c5345f7b1efe67eac328bc49e45a84c8b9ad
kynyka <congsu.yeh@gmail.com>
2018-04-23 10:28:21 +0800

    link to page: new_topic

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -7,4 +7,4 @@ class TopicForm(forms.ModelForm):  # 定义了一个名为TopicForm的类,它继
     class Meta:  # 最简单的ModelForm版本只包含一个内嵌的Meta类,它告诉Django根据哪个模型创建表单,以及在表单中包含哪些字段。
         model = Topic  # 根据模型Topic创建一个表单
         fields = ['text']  # 该表单只包含字段text
-        labels = {'text': ''}  # 让Django不要为字段text生成标签
+        labels = {'text': ''}  # 让Django不要为字段text生成标签,即input框前没字
