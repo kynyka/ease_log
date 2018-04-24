7615a995e5d7892ac430107f9da8db1f85334136
kynyka <congsu.yeh@gmail.com>
2018-04-24 22:47:26 +0800

    create user account: app users: URLs that include users app

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^users/', include('users.urls', namespace='users')),  #任何以单词users打头的URL（如http://localhost:8000/users/login/）都能匹配。创建命名空间'users',以便将应用程序 learning_logs 的URL同应用程序 users 的URL区分开来
     url(r'', include('learning_logs.urls', namespace='learning_logs')),  # 实参namespace让我们能够将learning_logs的URL同项目中的其他URL区分开来
 ]
