d84a9a0c48d73d5efea0de0c68f6d2bdab4bd384
kynyka <congsu.yeh@gmail.com>
2018-04-25 14:25:42 +0800

    let users have their own data > refer data with users: modify Topic model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User  # 首先导入django.contrib.auth中的模型User
 
 # Create your models here.
 class Topic(models.Model):
     '''用户学习的主题'''
     text = models.CharField(max_length=200)  # charfield必须告知django该在数据库中预留多少空间
     date_added = models.DateTimeField(auto_now_add=True)  # 自动设置成当前日期和时间;见官网: ... ref/models/fields/
+    owner = models.ForeignKey(User)  # 然后在Topic中添加字段owner,它建立到模型User的外键关系
 
     def __unicode__(self):  # Py 3.X用__str__()
         '''返回模型的字符串表示'''
